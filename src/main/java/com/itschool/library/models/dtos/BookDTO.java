a1a4045bcdccc681167b427483fc5dd2d62b29da
MihneaCatoiu <mihneaaureliancatoiu@gmail.com>
2024-09-09 21:02:24 +0300

    2nd commit.

---
+++ src/main/java/com/itschool/library/models/dtos/BookDTO.java
@@ -0,0 +1,16 @@
+package com.itschool.library.models.dtos;
+
+
+import jakarta.persistence.Column;
+import lombok.Data;
+
+@Data
+public class BookDTO {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private String genre;
+    private int copiesAvailable;
+}
